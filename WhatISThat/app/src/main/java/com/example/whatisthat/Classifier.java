c09d54dbb5aa51e1ab22cab2a4d3c51ecd592300
Alvaro Arenas <alvaare@gmail.com>
2021-10-21 18:40:25 +0200

    remove package errors

--- WhatISThat/app/src/main/java/com/example/whatisthat/Classifier.java
+++ WhatISThat/app/src/main/java/com/example/whatisthat/Classifier.java
@@ -1,69 +1,66 @@
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
+package com.example.whatisthat;
 
-import java.io.File;
-import java.io.IOException;
-import org.tensorflow.Graph;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
-import javax.imageio.ImageIO;
+import androidx.annotation.NonNull;
 
+import java.util.Random;
+
+//import org.tensorflow.Graph;
 import org.tensorflow.Tensor;
 
 public class Classifier {
 	private final int WIDTH = 299;
 	private final int HEIGHT = 299;
 	
-	private BufferedImage image;
-	private Tensor<Float> imageTensor;
+	private Bitmap image;
+	private Tensor imageTensor;
 	
-	public Classifier(String imageFile) {
-		readImage(imageFile);
-		resizeImage();
+	public Classifier() {
+		//readImage(imageFile);
+		//resizeImage();
 		createTensor();
 	}
 	
 	private void readImage(String filename) {
-		File inputFile = new File("src/main/resources/images/"+filename);
-		try {
-			image = ImageIO.read(inputFile);
-		} catch (IOException e) {
-			System.out.println("Image " + filename + " could not be read");
-			e.printStackTrace();
-            System.exit(1);
-		}
+		String IMAGESPATH = "";
+		String pathName = IMAGESPATH+filename;
+		image = BitmapFactory.decodeFile(pathName);
 	}
 	
 	private void createTensor() {
-		int width = image.getWidth();
-        int height = image.getHeight();
-        int[] data = new int[width * height];
-        image.getRGB(0, 0, width, height, data, 0, width);
-        float[][][][] rgbArray = new float[1][height][width][3];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                Color color = new Color(data[i * width + j]);
-                rgbArray[0][i][j][0] = color.getRed();
-                rgbArray[0][i][j][1] = color.getGreen();
-                rgbArray[0][i][j][2] = color.getBlue();
+        int[] data = new int[WIDTH * HEIGHT];
+        float[][][][] rgbArray = new float[1][HEIGHT][WIDTH][3];
+        //image.getRGB(0, 0, WIDTH, HEIGHT, data, 0, width);
+		Random rand = new Random();
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                //Color color = new Color(data[i * WIDTH + j]);
+                //rgbArray[0][i][j][0] = color.getRed();
+				rgbArray[0][i][j][0] = rand.nextFloat()*255;
+                //rgbArray[0][i][j][1] = color.getGreen();
+				rgbArray[0][i][j][1] = rand.nextFloat()*255;
+                //rgbArray[0][i][j][2] = color.getBlue();
+				rgbArray[0][i][j][2] = rand.nextFloat()*255;
             }
         }
         imageTensor = Tensor.create(rgbArray, Float.class);
 	}
 	
 	private void resizeImage() {
-		BufferedImage tempImage = new BufferedImage(WIDTH, HEIGHT, image.getType());  
+		/*BufferedImage tempImage = new BufferedImage(WIDTH, HEIGHT, image.getType());
 	    Graphics2D g = tempImage.createGraphics();  
 	    g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, 
 	    		RenderingHints.VALUE_INTERPOLATION_BILINEAR);  
 	    g.drawImage(image, 0, 0, WIDTH, HEIGHT, 0, 0, image.getWidth(), image.getHeight(), null);  
-	    image = tempImage;
+	    image = tempImage;*/
 	}
 	
-
+	@NonNull
+	@Override
 	public String toString() {
-		return imageTensor.toString();
+		return image.toString();
 	}
 	
 	public void run() {
